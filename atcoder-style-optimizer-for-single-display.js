345677355c86b8fbf21329eb31a5921f0555564a
xe-o <41024231+xe-o@users.noreply.github.com>
2023-04-09 15:06:02 +0900

    add: atcoder-style-optimizer-for-single-display.js

---
+++ atcoder-style-optimizer-for-single-display.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            Style optimizer for single display
+// @name:ja         Style optimizer for single display
+// @namespace       https://github.com/xe-o
+// @version         0.1
+// @description     Overrides AtCoder styles for single display
+// @description:ja  シングルディスプレイ向けにAtCoderのスタイルを最適化します
+// @author          XERO
+// @license         MIT
+// @match           https://atcoder.jp/*
+// @grant           none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const css = `
+    .container {
+      width: 100%;
+    }
+    .float-container > #main-container {
+      box-shadow: none;
+    }
+    #main-div.float-container {
+      background: none;
+    }
+  `;
+
+  const style = document.createElement("style");
+  style.textContent = css;
+  document.head.appendChild(style);
+})();
